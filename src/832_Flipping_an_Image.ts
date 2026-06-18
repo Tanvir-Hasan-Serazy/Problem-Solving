65095de607b8e38be0b21d13a6c37d586b4cac2c
Tanvir Hasan Serazy <amihasan420@gmail.com>
2026-06-18 15:58:17 +0600

    Add implementation for Flip and Invert Image algorithm in TypeScript

---
+++ src/832_Flipping_an_Image.ts
@@ -0,0 +1,17 @@
+function flipAndInvertImage(image: number[][]): number[][] {
+  for (let i = 0; i < image.length; i++) {
+    image[i].reverse();
+    for (let j = 0; j < image[i].length; j++) {
+      image[i][j] = image[i][j] === 0 ? 1 : 0;
+    }
+  }
+
+  return image;
+}
+
+const image = [
+  [1, 1, 0],
+  [1, 0, 1],
+  [0, 0, 0],
+];
+console.log(flipAndInvertImage(image));
