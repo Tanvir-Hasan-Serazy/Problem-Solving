e146064a837466b2bafd54422cd8649295ecc0d6
Tanvir Hasan Serazy <amihasan420@gmail.com>
2026-06-24 23:15:11 +0600

    Add implementation for Subarray Sum Equals K algorithm in TypeScript

---
+++ src/560_Subarray_Sum_Equals_K.ts
@@ -0,0 +1,16 @@
+const subarraySum = (nums: number[], k: number): number => {
+  const prefixCounts = new Map<number, number>([[0, 1]]);
+  let prefixSum = 0;
+  let count = 0;
+
+  for (const num of nums) {
+    prefixSum += num;
+    count += prefixCounts.get(prefixSum - k) ?? 0;
+    prefixCounts.set(prefixSum, (prefixCounts.get(prefixSum) ?? 0) + 1);
+  }
+
+  return count;
+};
+
+const nums = [1, 1, 1];
+const k = 2;
